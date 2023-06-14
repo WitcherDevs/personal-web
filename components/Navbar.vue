82d0d24d388568bd5185c454e9c641fd0be239ef
The Witcher <82153670+TheWTCHR@users.noreply.github.com>
2023-06-14 16:32:38 +0300

    Add navbar

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,1 +1,19 @@
-<template></template>
+<template>
+  <nav class="flex flex-row justify-end text-white w-[102rem] text-3xl gap-16 mt-[0.8rem]">
+    <NuxtLink exact-active-class="active" to="/" class="px-2 py-1 h-12 hover:text-red-500">
+      Home
+    </NuxtLink>
+    <NuxtLink exact-active-class="active" to="/about" class="px-2 py-1 h-12 hover:text-red-500">
+      About
+    </NuxtLink>
+    <NuxtLink exact-active-class="active" to="/references" class="px-2 py-1 h-12 hover:text-red-500">
+      References
+    </NuxtLink>
+  </nav>
+</template>
+
+<style>
+.active {
+  @apply bg-red-600/90 rounded-lg;
+}
+</style>
