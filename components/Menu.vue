b1a72e00903ae44f633ca66bc885b25863cb4f44
The Witcher <82153670+TheWTCHR@users.noreply.github.com>
2023-06-14 13:57:38 +0300

    Some menu changes

--- components/Menu.vue
+++ components/Menu.vue
@@ -1,9 +1,10 @@
 <template>
   <section>
     <div class="w-[11rem] h-screen bg-[#464646] flex flex-col text-center">
-      <h1 class="text-center text-[2rem] text-red-600/90 font-extrabold mt-2">WITCHER</h1>
-      <h3 class="rotatetext mt-[4.4rem] rotate-180 text-white text-[3.5rem] mx-auto font-thin ml-auto">
-        The best until we do better.
+      <h1 class="text-center text-[2rem] text-red-600/90 font-extrabold mt-[0.6rem]">WITCHER</h1>
+      <h3
+        class="rotatetext mt-[4.4rem] rotate-180 text-white text-[2.5rem] uppercase tracking-wider mx-auto font-thin ml-auto">
+        The best until we do better
       </h3>
       <div class="grid grid-cols-3 mt-[4.4rem] mx-auto gap-3">
         <a href="https://github.com/WitcherDevs">
@@ -16,7 +17,7 @@
           <Icon name="mdi:email" size="48" color="white" />
         </a>
       </div>
-      <h6 class="mt-auto mb-2 text-white w-24 mx-auto">Web design by
+      <h6 class="mt-auto mb-[0.6rem] text-white w-24 mx-auto font-serif">Web design by
         <span class="text-red-600/90 font-bold">Witcher</span>
       </h6>
     </div>
