828026eccce9e3a428f1d1b6526a303d6dfd0e8c
The Witcher <82153670+TheWTCHR@users.noreply.github.com>
2023-06-15 12:14:16 +0300

    Add meta tags and change icon

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,6 +5,23 @@ export default defineNuxtConfig({
   ],
 
   app: {
+    head: {
+      title: "Witcher Personal",
+      meta: [
+        { name: "name", content: "Witcher Personal"},
+        { name: "keywords", content: "Witcher, Developer, Developer Witcher, The Witcher" },
+        { name: "viewport", content: "width=device-width, initial-scale=1" },
+        { name: "theme-color", content: "#c92a2a" },
+        { name: "description", content: "Witcher Personal Website, projects, about and more..." },
+        { name: "og:title", content: "Witcher Personal" },
+        { name: "og:description", content: "Witcher Personal Website, projects, about and more..." },
+        { name: "og:type", content: "website" },
+        { name: "og:url", content: "https://witcherdev.net" },
+        { name: "og:image", content: "https://witcherdev.net/favicon.ico" },
+        { name: "copyright", content: "Witcher"},
+        { name: "author", content: "Witcher" },
+      ]
+    },
     pageTransition: { name: "page", mode: "out-in" },
   },
 
