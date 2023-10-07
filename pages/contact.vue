bfe07c2520d868ef3312527b4a50f97e6bb62d24
Witcher <cagrihaan1994@gmail.com>
2023-10-07 10:55:53 +0300

    Change card design

--- pages/contact.vue
+++ pages/contact.vue
@@ -17,7 +17,8 @@ const platforms = ref([{
 <template>
   <main class="flex justify-center items-center h-[180vh] md:h-[82vh]">
     <div class="grid grid-cols-1 md:mt-0 md:grid-cols-3 gap-32">
-      <div v-for="(item) in  platforms " class="bg-neutral-800 h-[25rem] w-[24rem] flex rounded-lg flex-col items-center">
+      <div v-for="(item) in  platforms "
+        class="bg-neutral-800 h-[25rem] w-[24rem] flex rounded-lg flex-col items-center border border-red-700">
         <Icon :name="item.icon" size="100" class="mt-12" />
         <h2 class="text-4xl font-bold mt-8">{{ item.name }}</h2>
         <h4 class="mt-4 text-lg">⤷ {{ item.value }}</h4>
